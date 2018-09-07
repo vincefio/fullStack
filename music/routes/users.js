ca1cda6ef91f88be5f5e2c4c36253909cb3ed1ca
Vince <vincentfiorilli@gmail.com>
2018-09-07 05:29:15 -0400

    going back and forth

--- music/routes/users.js
+++ music/routes/users.js
@@ -6,10 +6,21 @@ router.get('/register', function (req, res) {
     res.render('register');
 });
 
-// Register
+// Login
 router.get('/login', function (req, res) {
     res.render('login');
 });
+
+// Register User
+/*router.post('/register', function (req, res) {
+    var name = req.body.name;
+    var email = req.body.email;
+    var username = req.body.username;
+    var password = req.body.password;
+    var password2 = req.body.password2;
+
+    console.log(name)
+});*/
 
 
 module.exports = router;
