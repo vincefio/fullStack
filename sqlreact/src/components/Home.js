bc3366c003ceea513321fd448fbe6e16c51d87ee
Vince <vincentfiorilli@gmail.com>
2018-09-19 17:53:11 -0400

    yo

--- sqlreact/src/components/Home.js
+++ sqlreact/src/components/Home.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react'
 export default class Home extends Component {
     render() {
         return (
-            <div>
-                <h1>Hello {this.props.user}</h1>
+            <div >
+                <h3>Hello {this.props.user}</h3>
+                <div className="divider"></div>
+                <h3>Products</h3>
             </div>
         )
     }
