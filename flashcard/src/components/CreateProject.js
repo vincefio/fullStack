8a04e80831fb02c806225426ee0c78fc1964c4ae
Vince <vincentfiorilli@gmail.com>
2018-10-14 13:12:14 -0400

    modal functionality

--- flashcard/src/components/CreateProject.js
+++ flashcard/src/components/CreateProject.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import { Button, Icon, Modal } from 'react-materialize'
+
+
 export default class CreateProject extends Component {
 
     handleSubmit(event) {
@@ -26,8 +29,19 @@ export default class CreateProject extends Component {
                         Project Name:
                         <input type="text" name="name" />
                     </label>
+
+                    <Modal
+                        header='Modal Header'
+                        trigger={<Button><i className="fas fa-plus"></i>Add FlashCard</Button>}>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                    </Modal>
+                    <br />
+
                     <input type="submit" value="Submit" />
                 </form>
+
+
+
 
             </div>
         )
