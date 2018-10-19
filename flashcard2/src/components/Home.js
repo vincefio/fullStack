6f013416297d98f8815cb81d2701569db09ac429
Vince <vincentfiorilli@gmail.com>
2018-10-19 19:20:44 -0400

    css animations

--- flashcard2/src/components/Home.js
+++ flashcard2/src/components/Home.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
+import { Button } from 'react-materialize'
 
 export default class Home extends Component {
     render() {
         return (
             <div className='App-header'>
-                <h1>OMG itsssss FlashCard!</h1>
-
+                <h1 id="header-text">OMG itsssss...</h1>
+                <div className="header-container">
+                    <h2 id="header-text2">FlashCard!</h2>
+                </div>
+                <div className="button-container">
+                    <Button className='button btn-large red' waves='light'>Study</Button>
+                    <Button className='button btn-large' waves='light'>Create New</Button>
+                </div>
             </div>
         )
     }
