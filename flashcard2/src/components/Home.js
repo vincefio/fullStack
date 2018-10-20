358ac6712533a5d333ba9f98b85c4d847b97e0d7
Vince <vincentfiorilli@gmail.com>
2018-10-20 04:38:36 -0400

    home component done

--- flashcard2/src/components/Home.js
+++ flashcard2/src/components/Home.js
@@ -1,8 +1,37 @@
 import React, { Component } from 'react'
 import { Button } from 'react-materialize'
+import { withRouter } from 'react-router-dom'
+
+
+
 
 export default class Home extends Component {
+
     render() {
+        const Button1 = withRouter(({ history }) => (
+            <button
+                type='button'
+                className='button btn-large red'
+                waves='light'
+                onClick={() => { history.push('/projects') }}
+            >
+                Study
+            </button>
+        ))
+
+        const Button2 = withRouter(({ history }) => (
+            <button
+                type='button'
+                className='button btn-large'
+                waves='light'
+                onClick={() => { history.push('/addproject') }}
+            >
+                Create New
+            </button>
+        ))
+
+
+
         return (
             <div className='App-header'>
                 <h1 id="header-text">OMG itsssss...</h1>
@@ -10,8 +39,8 @@ export default class Home extends Component {
                     <h2 id="header-text2">FlashCard!</h2>
                 </div>
                 <div className="button-container">
-                    <Button className='button btn-large red' waves='light' href="/projects">Study</Button>
-                    <Button className='button btn-large' waves='light'>Create New</Button>
+                    <Button1 />
+                    <Button2 />
                 </div>
             </div>
         )
