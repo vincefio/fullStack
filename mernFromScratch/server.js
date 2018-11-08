a49ae6a5cbcea45996fed265e0adce49210e69e8
Vince <vincentfiorilli@gmail.com>
2018-11-08 05:26:12 -0500

    less gooo

---
+++ mernFromScratch/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+
+const app = express()
+const port = 8080
+
+//initialize body parser
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+var db = require('./db/config/keys').mongoURI
+
+// Connect to Mongo
+mongoose
+    .connect(db, { useNewUrlParser: true }) // Adding new mongo url parser
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
