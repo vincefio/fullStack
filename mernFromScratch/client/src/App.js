2fabee85f9dd2cb25a455ff17664438d531574c1
Vince <vincentfiorilli@gmail.com>
2018-11-08 06:17:08 -0500

    router

--- mernFromScratch/client/src/App.js
+++ mernFromScratch/client/src/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Navbar from './components/Navbar'
+import Home from './components/Home'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1>oh shittt</h1>
-        </header>
-      </div>
+      <Router>
+        <div>
+          <Navbar />
+          <div className="App">
+            <header className="App-header">
+              <h1>oh shittt</h1>
+            </header>
+          </div>
+          {/*Routing*/}
+          <Route exact path="/" component={Home} />
+        </div>
+
+      </Router>
     );
   }
 }
---
+++ mernFromScratch/client/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <h1>nav</h1>
+            </div>
+        )
+    }
+}
