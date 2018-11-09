ef9fd65fd5edb88960f6e38ea4b269b4dd02bf80
Vince <vincentfiorilli@gmail.com>
2018-11-09 06:15:59 -0500

    saving category to db

--- mernFromScratch/routes/index.js
+++ mernFromScratch/routes/index.js
@@ -1,11 +1,17 @@
 var express = require('express')
 var router = express.Router()
+const Category = require('../db/models/Category')
 
 router.get('/', (req, res) => res.send('Hello World!'))
 
 router.post('/category', (req, res) => {
-    console.log(req.body)
-    res.send('route hit')
+    var newCat = new Category(req.body)
+
+    newCat.save(function (err) {
+        if (err) return handleError(err)
+    })
+
+    res.send('document saved!')
 })
 
 module.exports = router;
