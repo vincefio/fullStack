921384d42d1128a8a1713b00baccfab09d8adf89
Vince <vincentfiorilli@gmail.com>
2018-11-10 06:38:39 -0500

    working w axios, navbar dropdown no good

--- mernFromScratch/client/src/components/Categories.js
+++ mernFromScratch/client/src/components/Categories.js
@@ -2,11 +2,26 @@ import React, { Component } from 'react'
 import axios from 'axios'
 
 export default class Categories extends Component {
+    constructor() {
+        super()
+        this.state = ({
+            categories: []
+        })
+        this.componentDidMount = this.componentDidMount.bind(this)
+    }
+
     componentDidMount() {
         //request category info from db
+
+        //save reference to this to set state inside axios call
+        let self = this
+
         axios.get('/categories')
             .then(function (response) {
-                console.log(response);
+                console.log(response.data);
+                self.setState({
+                    categories: response.data
+                })
             })
             .catch(function (error) {
                 console.log(error);
@@ -16,7 +31,7 @@ export default class Categories extends Component {
     render() {
         return (
             <div>
-                <h1>categories</h1>
+                <h4>categories</h4>
             </div>
         )
     }
--- mernFromScratch/client/src/components/Navigation.js
+++ mernFromScratch/client/src/components/Navigation.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react'
-import { Navbar, NavItem } from 'react-materialize'
+import { Navbar, NavItem, Link } from 'react-materialize'
 import M from 'materialize-css'
 
 export default class Navigation extends Component {
-    componentDidMount() {
-        let elems = document.querySelectorAll('.dropdown-trigger');
-        M.Dropdown.init(elems, { inDuration: 300, outDuration: 225 });
+    componentWillMount() {
+        var elems = document.querySelectorAll('.dropdown-trigger');
+        M.Dropdown.init(elems, { inDuration: 300, outDuration: 225 })
     }
 
     render() {
         return (
+
             <div>
                 <nav>
-                    <div className="nav-wrapper">
+                    <div className="nav-wrapper  blue-grey darken-2">
                         <a href="#" className="brand-logo">Logo</a>
+                        <a className='dropdown-trigger hide-on-large-only' href='#' data-target='dropdown1'><i className='material-icons'>dehaze</i></a>
+                        <ul id="nav-mobile" className="right ">
+
+                            <li><a href="sass.html">Sass</a></li>
+                            <li><a href="badges.html">Components</a></li>
+                            <li><a href="collapsible.html">JavaScript</a></li>
+
+
+                        </ul>
+
                         <ul id="nav-mobile" className="right hide-on-med-and-down">
                             <li><a href="sass.html">Sass</a></li>
                             <li><a href="badges.html">Components</a></li>
