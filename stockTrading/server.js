f2f8f5a6860a605b02b299473d47aeee86986a61
Vince <vincentfiorilli@gmail.com>
2018-11-16 05:24:16 -0500

    server and db

--- stockTrading/server.js
+++ stockTrading/server.js
@@ -1,7 +1,14 @@
 const express = require('express')
+var bodyParser = require('body-parser')
+
 const app = express()
 const port = process.env.PORT || 8080
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
+//db connection
+var db = require('./db/connection')
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
