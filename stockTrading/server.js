d73195a0c48c7b70d76bc91552281e855b76143b
Vince <vincentfiorilli@gmail.com>
2018-12-13 20:21:29 -0500

    ready for heroku deployment

--- stockTrading/server.js
+++ stockTrading/server.js
@@ -14,4 +14,14 @@ var db = require('./db/connection')
 //require routes
 app.use('/', require('./routes'))
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
